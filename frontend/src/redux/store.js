6c0ea17ca703befcb058ebbac69ba18d46ed1d4a
Jakub Cieślik <kuba8172@gmail.com>
2024-02-15 12:34:25 +0100

    created redux store

---
+++ frontend/src/redux/store.js
@@ -0,0 +1,15 @@
+import { configureStore } from '@reduxjs/toolkit';
+import { apiSlice } from '../services/apiSlice';
+
+const store = configureStore({
+  reducer: {
+    [apiSlice.reducerPath]: apiSlice.reducer,
+  },
+  middleware: (getDefaultMiddleware) => {
+    return getDefaultMiddleware({
+      serializableCheck: false,
+    }).concat(apiSlice.middleware);
+  },
+});
+
+export default store;
