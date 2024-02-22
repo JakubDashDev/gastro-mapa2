ef0d455adb37edbb92899910491fd2d3d308d682
Jakub Cieślik <kuba8172@gmail.com>
2024-02-22 20:37:24 +0100

    get data & display Loader, SideNav and Map

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,9 +1,38 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react';
+import Map from './components/features/Map';
+import { useGetRestaurantsQuery } from './services/restaurantsApi';
+import Loader from './components/ui/Loader';
+import SideNav from './components/features/SideNav';
 
 function App() {
+  const { loading, data, error } = useGetData();
+
+  if (loading) return <Loader />;
+
   return (
-    <div>App</div>
-  )
+    <main className="flex font-sans">
+      <SideNav data={data} />
+      <div className="w-4/5 h-screen border-y-4 border-e-4 border-white">
+        <Map data={data} />
+      </div>
+    </main>
+  );
 }
 
-export default App
+export default App;
+
+function useGetData() {
+  const { data, isLoading, error } = useGetRestaurantsQuery();
+
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    if (!isLoading) {
+      setTimeout(() => {
+        setLoading(false);
+      }, 3000);
+    }
+  }, [isLoading]);
+
+  return { loading, data, error };
+}
