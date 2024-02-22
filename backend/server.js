5eef024c1fe59fb2884a51b414e4d7da260fb51f
Jakub Cieślik <kuba8172@gmail.com>
2024-02-22 20:49:05 +0100

    connect to DB, restaurant routes and errorHandler

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,24 @@
 import express from 'express';
 import morgan from 'morgan';
+import dotenv from 'dotenv';
+dotenv.config({ path: './.env' });
+import connectDB from './config/db.js';
+import { errorHandler } from './middleware/errorMiddleware.js';
+import restaurantRoutes from './routes/restaurantsRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
 
-const asnycHandler = (fn) => (req, res, next) => {
-  Promise.resolve(fn(req, res, next)).catch(next);
-};
+connectDB();
+
+app.use('/api/restaurants', restaurantRoutes);
+
+app.use(errorHandler);
+
+app.listen(process.env.PORT, () => {
+  console.log(
+    `Server is running on port ${process.env.PORT} as ${process.env.NODE_ENV}`
+  );
+});
