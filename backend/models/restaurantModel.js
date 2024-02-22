89eb753009800c2c64f0e2256b2d899c902ec694
Jakub Cieślik <kuba8172@gmail.com>
2024-02-22 20:10:44 +0100

    restaurant model

---
+++ backend/models/restaurantModel.js
@@ -0,0 +1,33 @@
+import mongoose, { Schema } from 'mongoose';
+
+const addressSchema = new mongoose.Schema({
+  street: { type: String, required: true },
+  houseNumber: { type: Schema.Types.Mixed, required: true },
+  city: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  street: { type: String, required: true },
+  country: { type: String, required: true },
+});
+
+const latLngSchema = new mongoose.Schema({
+  lat: { type: Number, required: true },
+  lng: { type: Number, required: true },
+});
+
+const youtubeRefSchema = new mongoose.Schema({
+  link: { type: String, required: true },
+  timestamp: { type: Number },
+});
+
+const restaurantSchema = new mongoose.Schema({
+  name: { type: String, required: true, unique: true },
+  address: addressSchema,
+  type: [String],
+  latlng: latLngSchema,
+  rating: { type: Schema.Types.Mixed, required: true },
+  youtubeRef: youtubeRefSchema,
+});
+
+const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+
+export default Restaurant;
