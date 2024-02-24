d18d7b33d39a9cd5cacee7567f92108072cbee59
Jakub Cieślik <kuba8172@gmail.com>
2024-02-24 01:34:02 +0100

    keyword feature

--- frontend/src/services/restaurantsApi.js
+++ frontend/src/services/restaurantsApi.js
@@ -4,9 +4,10 @@ import { apiSlice } from './apiSlice';
 export const restaurantsApi = apiSlice.injectEndpoints({
   endpoints: (builder) => ({
     getRestaurants: builder.query({
-      query: () => ({
-        url: 'http://localhost:3000/api/restaurants',
+      query: ({ keyword }) => ({
+        url: `http://localhost:3000/api/restaurants`,
         method: 'GET',
+        params: { keyword },
       }),
     }),
   }),
