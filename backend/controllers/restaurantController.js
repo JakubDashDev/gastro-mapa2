df6cb80bde8af58e341f0b5089926ae2bf66f576
Jakub Cieślik <kuba8172@gmail.com>
2024-02-24 01:28:17 +0100

    keyword feature

--- backend/controllers/restaurantController.js
+++ backend/controllers/restaurantController.js
@@ -2,7 +2,28 @@ import asnycHandler from '../middleware/asyncHandler.js';
 import Restaurant from '../models/restaurantModel.js';
 
 const getAllRestaurants = asnycHandler(async (req, res) => {
-  const restaurants = await Restaurant.find();
+  const keyword = req.query.keyword;
+  const find = keyword
+    ? {
+        $or: [
+          {
+            name: { $regex: new RegExp('^' + keyword.toLowerCase(), 'i') },
+          },
+          {
+            type: { $regex: new RegExp('^' + keyword.toLowerCase(), 'i') },
+          },
+          {
+            'address.city': {
+              $regex: new RegExp('^' + keyword.toLowerCase(), 'i'),
+            },
+          },
+          {
+            rating: { $regex: new RegExp('^' + keyword.toLowerCase(), 'i') },
+          },
+        ],
+      }
+    : {};
+  const restaurants = await Restaurant.find(find);
 
   res.json(restaurants);
 });
