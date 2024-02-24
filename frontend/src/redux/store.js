93d1838ada9584a5bddffd33dee59752be8592eb
Jakub Cieślik <kuba8172@gmail.com>
2024-02-24 01:33:48 +0100

    restaurant store

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../services/apiSlice';
+import restaurantsSlice from './restaurantsSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
+    restaurants: restaurantsSlice,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
