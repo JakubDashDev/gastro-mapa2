c28d3dcd6f806b5a0fc5cf91d2c978861d06f641
Jakub Cieślik <kuba8172@gmail.com>
2024-02-24 01:29:32 +0100

    added MapProvider

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -4,11 +4,14 @@ import App from './App.jsx';
 import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
+import { MapProvider } from 'react-map-gl';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <MapProvider>
+        <App />
+      </MapProvider>
     </Provider>
   </React.StrictMode>
 );
