ac1993d95886e0bf9e30dde444e9b700b93b68d1
Jakub Cieślik <kuba8172@gmail.com>
2024-02-24 01:29:22 +0100

    moved getRestaurant function to useGetRestaurant hooks

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -3,17 +3,21 @@ import Map from './components/features/Map';
 import { useGetRestaurantsQuery } from './services/restaurantsApi';
 import Loader from './components/ui/Loader';
 import SideNav from './components/features/SideNav';
+import { useDispatch, useSelector } from 'react-redux';
+import { setRestaurants } from './redux/restaurantsSlice';
+import useGetRestaurants from './hooks/useGetRestaurants';
 
 function App() {
-  const { loading, data, error } = useGetData();
+  const { restaurants } = useSelector((state) => state.restaurants);
+  const { loading, error } = useGetData();
 
   if (loading) return <Loader />;
 
   return (
     <main className="flex font-sans">
-      <SideNav data={data} />
+      <SideNav data={restaurants} />
       <div className="w-4/5 h-screen border-y-4 border-e-4 border-white">
-        <Map data={data} />
+        <Map data={restaurants} />
       </div>
     </main>
   );
@@ -22,9 +26,13 @@ function App() {
 export default App;
 
 function useGetData() {
-  const { data, isLoading, error } = useGetRestaurantsQuery();
+  const { getRestaurants, isLoading, error } = useGetRestaurants();
 
   const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    getRestaurants({ keyword: undefined });
+  }, []);
 
   useEffect(() => {
     if (!isLoading) {
@@ -34,5 +42,5 @@ function useGetData() {
     }
   }, [isLoading]);
 
-  return { loading, data, error };
+  return { loading, error };
 }
