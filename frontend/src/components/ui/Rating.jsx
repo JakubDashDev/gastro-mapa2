928382457394ebe3c437a43145aa16cbe24a8daa
Jakub Cieślik <kuba8172@gmail.com>
2024-03-01 09:25:04 +0100

    UPDATE: if rating equals 5 then display text

--- frontend/src/components/ui/Rating.jsx
+++ frontend/src/components/ui/Rating.jsx
@@ -2,23 +2,12 @@ import React from 'react';
 import { FaStar, FaStarHalfAlt, FaRegStar } from 'react-icons/fa';
 
 function Rating({ rating, textSize }) {
-  if (rating === 'MUALA') {
-    return (
-      <div
-        className={`${textSize} flex items-center gap-1 text-primary-500 mt-[-5]`}
-      >
-        <FaStar />
-        <FaStar />
-        <FaStar />
-        <FaStar />
-        <FaStar />
-        <span className="font-loader mb-[-4px]">MUALA</span>
-      </div>
-    );
-  }
-
   return (
-    <div className="flex gap-1 items-center text-yellow-500 text-base font-bold">
+    <div
+      className={`flex gap-1 items-center text-base font-bold ${
+        rating === 5 ? 'text-primary-500' : 'text-yellow-500'
+      }`}
+    >
       <span>
         {rating >= 1 ? (
           <FaStar />
@@ -57,14 +46,20 @@ function Rating({ rating, textSize }) {
       </span>
       <span>
         {rating >= 5 ? (
-          <FaStar />
+          <div
+            className={`${textSize} flex items-center gap-1 text-primary-500 mt-[-5]`}
+          >
+            <FaStar />
+          </div>
         ) : rating >= 4.5 ? (
           <FaStarHalfAlt />
         ) : (
           <FaRegStar />
         )}
       </span>
-      <span className="mx-1">{rating}</span>
+      <span className={`mx-1 ${rating === 5 ? 'font-loader' : 'font-sans'}`}>
+        {rating === 5 ? 'MUALA' : rating}
+      </span>
     </div>
   );
 }
