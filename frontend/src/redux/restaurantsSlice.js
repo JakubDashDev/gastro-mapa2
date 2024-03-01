511e9078918fd675faf3dd1fb18fac46f7fd3ca1
Jakub Cieślik <kuba8172@gmail.com>
2024-03-01 09:24:10 +0100

    NEW: sort functions

--- frontend/src/redux/restaurantsSlice.js
+++ frontend/src/redux/restaurantsSlice.js
@@ -7,9 +7,28 @@ const restaurantSlice = createSlice({
     setRestaurants: (state, action) => {
       state.restaurants = action.payload;
     },
+    sortRestaurants: (state, action) => {
+
+      if (action.payload === 'Alfabetycznie (A-Z)') {
+        state.restaurants = state.restaurants
+          .slice()
+          .sort((a, b) => a.name.localeCompare(b.name));
+      }
+      if (action.payload === 'Alfabetycznie (Z-A)') {
+        state.restaurants = state.restaurants
+          .slice()
+          .sort((a, b) => -1 * a.name.localeCompare(b.name));
+      }
+      if (action.payload === 'Ocena: malejąco') {
+        state.restaurants = state.restaurants.slice().sort((a, b) => a.rating - b.rating);
+      }
+      if (action.payload === 'Ocena: rosnąco') {
+        state.restaurants = state.restaurants.slice().sort((a, b) => b.rating - a.rating);
+      }
+    },
   },
 });
 
-export const { setRestaurants } = restaurantSlice.actions;
+export const { setRestaurants, sortRestaurants } = restaurantSlice.actions;
 
 export default restaurantSlice.reducer;
