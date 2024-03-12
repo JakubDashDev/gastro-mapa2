efcc4d25ffd261e3bdf6a180babf97d97237fa81
Jakub Cieślik <kuba8172@gmail.com>
2024-03-12 11:25:59 +0100

    NEW: display amount of active filters

--- frontend/src/components/features/FilterButton.jsx
+++ frontend/src/components/features/FilterButton.jsx
@@ -1,14 +1,22 @@
 import React, { createContext, useEffect, useState } from 'react';
 import { FaFilter } from 'react-icons/fa';
 import FilterSideNav from '../ui/FilterSideNav';
+import { useSelector } from 'react-redux';
 
 function FilterButton() {
   const [isOpen, setIsOpen] = useState(false);
+  const { filterQuery } = useSelector((state) => state.restaurants);
   return (
     <div className="flex flex-col justify-center items-center gap-1">
-      <span className="text-gray-500">Filtry</span>
-      <button onClick={() => setIsOpen((prev) => !prev)}>
-        <FaFilter className="text-lg text-gray-600" />
+      <div className="text-gray-500 flex items-center gap-1">
+        <span>Filtry</span>
+        {filterQuery.length > 0 && <span className='text-red-600'>({filterQuery.length})</span>}
+      </div>
+      <button
+        onClick={() => setIsOpen((prev) => !prev)}
+        className="text-lg text-gray-600"
+      >
+        <FaFilter />
       </button>
 
       {isOpen && <FilterSideNav setIsOpen={setIsOpen} />}
