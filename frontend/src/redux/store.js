651432ec388d1dc5596324f12ce1ba420a958629
Jakub Cieślik <kuba8172@gmail.com>
2024-03-17 19:11:24 +0100

    NEW: auth slice

--- frontend/src/redux/store.js
+++ frontend/src/redux/store.js
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import { apiSlice } from '../services/apiSlice';
 import restaurantsSlice from './restaurantsSlice';
 import filtersSlice from './filtersSlice';
+import authSlice from './authSlice';
 
 const store = configureStore({
   reducer: {
     [apiSlice.reducerPath]: apiSlice.reducer,
     restaurants: restaurantsSlice,
     filters: filtersSlice,
+    auth: authSlice,
   },
   middleware: (getDefaultMiddleware) => {
     return getDefaultMiddleware({
