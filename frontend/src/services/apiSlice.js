f15b85492260baacb3ebdef16e048a164ee248cf
Jakub Cieślik <kuba8172@gmail.com>
2024-03-17 19:11:48 +0100

    UPDATE: credentials: include

--- frontend/src/services/apiSlice.js
+++ frontend/src/services/apiSlice.js
@@ -1,21 +1,24 @@
 import { fetchBaseQuery, createApi } from '@reduxjs/toolkit/query/react';
 import { BASE_URL } from '../../constatns';
+import { removeAuth } from '../redux/authSlice';
 
 const baseQuery = fetchBaseQuery({
   baseUrl: BASE_URL,
+  credentials: 'include'
 });
 
 async function baseQueryWithAuth(args, api, extra) {
   const result = await baseQuery(args, api, extra);
   // Dispatch the logout action on 401.
   if (result.error && result.error.status === 401) {
-    // api.dispatch(logout());
+    // api.dispatch(removeAuth());
     console.log("error 401")
   }
   return result;
 }
 
 export const apiSlice = createApi({
-  baseQuery: baseQuery,
+  baseQuery: baseQueryWithAuth,
   endpoints: (builder) => ({}),
+  
 });
