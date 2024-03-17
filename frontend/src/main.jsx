313ae98e691279d27fb093dea489434142bace16
Jakub Cieślik <kuba8172@gmail.com>
2024-03-17 19:10:08 +0100

    NEW: routes

--- frontend/src/main.jsx
+++ frontend/src/main.jsx
@@ -5,12 +5,37 @@ import './index.css';
 import { Provider } from 'react-redux';
 import store from './redux/store.js';
 import { MapProvider } from 'react-map-gl';
+import { RouterProvider, createBrowserRouter } from 'react-router-dom';
+import AdminRoute from './components/AdminRoute.jsx';
+import RestaurantList from './pages/admin/HomePage.jsx';
+import LoginPage from './pages/admin/LoginPage.jsx';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <App />,
+  },
+  {
+    path: '/admin/main',
+    element: <AdminRoute />,
+    children: [
+      {
+        path: '',
+        element: <RestaurantList />,
+      },
+    ],
+  },
+  {
+    path: '/admin/auth',
+    element: <LoginPage />,
+  },
+]);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Provider store={store}>
       <MapProvider>
-        <App />
+        <RouterProvider router={router} />
       </MapProvider>
     </Provider>
   </React.StrictMode>
