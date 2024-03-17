4ce0bf3a079166793efdf56ebb585e228e262a0f
Jakub Cieślik <kuba8172@gmail.com>
2024-03-17 19:05:39 +0100

    NEW: cors, cookies, admin routes

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,29 @@
 import express from 'express';
 import morgan from 'morgan';
+import cors from 'cors';
+import cookies from 'cookie-parser';
 import dotenv from 'dotenv';
 dotenv.config({ path: './.env' });
 import connectDB from './config/db.js';
 import { errorHandler } from './middleware/errorMiddleware.js';
 import restaurantRoutes from './routes/restaurantsRoutes.js';
+import adminRoutes from './routes/adminRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(morgan('dev'));
+app.use(
+  cors({
+    credentials: true,
+  })
+);
+app.use(cookies());
 
 connectDB();
 
 app.use('/api/restaurants', restaurantRoutes);
+app.use('/api/admin', adminRoutes);
 
 app.use(errorHandler);
 
