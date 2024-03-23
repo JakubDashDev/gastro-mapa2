d18356f58ae047f1fd0130548359771c67b8a6a9
Jakub Cieślik <kuba8172@gmail.com>
2024-03-23 23:06:00 +0100

    UPDATE: rm duplicate of street schema & changed youtubeRef to string & changed name from type to category and make it requied

--- backend/models/restaurantModel.js
+++ backend/models/restaurantModel.js
@@ -5,7 +5,6 @@ const addressSchema = new mongoose.Schema({
   houseNumber: { type: Schema.Types.Mixed, required: true },
   city: { type: String, required: true },
   zipCode: { type: String, required: true },
-  street: { type: String, required: true },
   country: { type: String, required: true },
 });
 
@@ -14,19 +13,17 @@ const latLngSchema = new mongoose.Schema({
   lng: { type: Number, required: true },
 });
 
-const youtubeRefSchema = new mongoose.Schema({
-  link: { type: String, required: true },
-  timestamp: { type: Number },
-});
-
-const restaurantSchema = new mongoose.Schema({
-  name: { type: String, required: true, unique: true },
-  address: addressSchema,
-  type: [String],
-  latlng: latLngSchema,
-  rating: { type: Number, required: true },
-  youtubeRef: youtubeRefSchema,
-});
+const restaurantSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true, unique: true },
+    address: addressSchema,
+    category: { type: [String], required: true },
+    latlng: latLngSchema,
+    rating: { type: Number, required: true },
+    youtubeRef: { type: String, required: true },
+  },
+  { timestamps: true }
+);
 
 const Restaurant = mongoose.model('Restaurant', restaurantSchema);
 
