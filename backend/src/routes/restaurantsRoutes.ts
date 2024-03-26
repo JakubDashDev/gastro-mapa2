7e98f7b2ed28f16f3b574deb1e72c1d623bb7f92
Jakub Cieślik <kuba8172@gmail.com>
2024-03-26 23:21:40 +0100

    UPDATE: getAllRestaurants for admin

--- backend/src/routes/restaurantsRoutes.ts
+++ backend/src/routes/restaurantsRoutes.ts
@@ -1,9 +1,10 @@
-import express from 'express';
-import { getAllRestaurants } from '../controllers/restaurantController.js';
+import express from "express";
+import { getAllRestaurants } from "../controllers/restaurantController.js";
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
-router.route('/').get(getAllRestaurants);
+router.route("/").get(getAllRestaurants);
+router.route("/admin").get(protect, getAllRestaurants);
 
-
-export default router
+export default router;
