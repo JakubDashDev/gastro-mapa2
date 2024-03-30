92fb235e8519bc261466fc7a29b4ea01df5319ab
Jakub Cieślik <kuba8172@gmail.com>
2024-03-30 13:33:08 +0100

    UPDATE: lower case

--- frontend/constatns.ts
+++ frontend/constatns.ts
@@ -1,19 +1,19 @@
-export const BASE_URL = 'http://localhost:3000';
+export const BASE_URL = "http://localhost:3000";
 
-export const RESTAURANT_URL = '/api/restaurants/';
+export const RESTAURANT_URL = "/api/restaurants/";
 
 export const CATEGORY_ARRAY = [
-  'Pizza',
-  'Kebab',
-  'Burger',
-  'Jarmark',
-  'Dziadkowie Bizensu',
-  'Restauracja',
-  'Słodycz',
-  'Zapiekanka',
-  'Chińczyk',
-  'Bar mleczny',
-  'Hot-dog',
-];
+  "pizza",
+  "kebab",
+  "burger",
+  "jarmark",
+  "dziadkowie bizensu",
+  "restauracja",
+  "słodycz",
+  "zapiekanka",
+  "chińczyk",
+  "bar mleczny",
+  "hot-dog",
+].sort((a, b) => a.localeCompare(b));
 
 export const RATING_ARRAY = [5, 4, 3, 2, 1, 0];
