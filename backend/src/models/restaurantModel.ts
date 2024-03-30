b74d028be0682fa3ef265f07bb0de9b03e8132ab
Jakub Cieślik <kuba8172@gmail.com>
2024-03-30 13:31:21 +0100

    UPDATE: rm houseNumber & changed latLng to array and moved to address object & added youtubeLink & youtubeRef -> youtubeEmbed

--- backend/src/models/restaurantModel.ts
+++ backend/src/models/restaurantModel.ts
@@ -1,16 +1,11 @@
-import mongoose, { Schema } from 'mongoose';
+import mongoose from "mongoose";
 
 const addressSchema = new mongoose.Schema({
   street: { type: String, required: true },
-  houseNumber: { type: Schema.Types.Mixed, required: true },
   city: { type: String, required: true },
   zipCode: { type: String, required: true },
   country: { type: String, required: true },
-});
-
-const latLngSchema = new mongoose.Schema({
-  lat: { type: Number, required: true },
-  lng: { type: Number, required: true },
+  latLng: { type: [Number], required: true },
 });
 
 const restaurantSchema = new mongoose.Schema(
@@ -18,13 +13,13 @@ const restaurantSchema = new mongoose.Schema(
     name: { type: String, required: true, unique: true },
     address: addressSchema,
     category: { type: [String], required: true },
-    latlng: latLngSchema,
     rating: { type: Number, required: true },
-    youtubeRef: { type: String, required: true },
+    youtubeEmbed: { type: String, required: true },
+    youtubeLink: { type: String, required: true },
   },
   { timestamps: true }
 );
 
-const Restaurant = mongoose.model('Restaurant', restaurantSchema);
+const Restaurant = mongoose.model("Restaurant", restaurantSchema);
 
 export default Restaurant;
