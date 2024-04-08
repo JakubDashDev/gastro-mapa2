7d60471a1bfb0a543ba3b75b715491778993a3c5
Jakub Cieślik <kuba8172@gmail.com>
2024-04-08 09:39:29 +0200

    UPDATE: handling GetRestaurantQuery & Create restaurant modal

--- frontend/src/pages/admin/RestaurantsPage.tsx
+++ frontend/src/pages/admin/RestaurantsPage.tsx
@@ -1,19 +1,43 @@
-import React from 'react';
-import RestaurantList from '../../components/admin/UI/RestaurantList';
-import { FaPlus } from 'react-icons/fa6';
-import RestaurantSearch from '../../components/admin/features/RestaurantSearch';
+import React, { Fragment, useState } from "react";
+import RestaurantList from "../../components/ui/RestaurantListAdmin";
+import { FaPlus } from "react-icons/fa6";
+import RestaurantSearch from "../../components/features/RestaurantSearch";
+import CreateRestaurantForm from "../../components/features/CreateRestaurantForm";
+import useGetRestaurantsAdmin from "../../hooks/useGetRestaurantsAdmin";
+import Loader from "../../components/ui/Loader";
 
 function RestaurantsPage() {
-  return (
-    <div className="flex flex-col gap-3 container mx-auto px-4">
-      <button className="flex items-center justify-center w-[150px] gap-2 py-1 bg-primary-500 rounded-lg text-white">
-        <FaPlus /> Dodaj
-      </button>
-      <div className="overflow-x-auto bg-neutral-50 dark:bg-neutral-700 rounded-lg shadow-xl">
-        <RestaurantSearch />
-        <RestaurantList />
+  const [showModal, setShowModal] = useState(false);
+
+  const { isLoading, error } = useGetRestaurantsAdmin({
+    keyword: undefined,
+    filters: undefined,
+  });
+
+  if (isLoading)
+    return (
+      <div className="w-full h-full flex items-center justify-center">
+        <Loader color="border-primary-500" />
       </div>
-    </div>
+    );
+  if (error) return <span>error...</span>;
+
+  return (
+    <Fragment>
+      <div className="flex flex-col gap-3 container mx-auto px-4">
+        <button
+          onClick={() => setShowModal(true)}
+          className="flex items-center justify-center w-[150px] gap-2 py-1 bg-primary-500 hover:bg-primary-400 transition-colors rounded-lg text-white"
+        >
+          <FaPlus /> Dodaj
+        </button>
+        <div className="overflow-x-auto bg-neutral-50 dark:bg-neutral-700 rounded-lg shadow-xl">
+          <RestaurantSearch />
+          <RestaurantList />
+        </div>
+      </div>
+      <CreateRestaurantForm isShow={showModal} setIsShow={setShowModal} />
+    </Fragment>
   );
 }
 
