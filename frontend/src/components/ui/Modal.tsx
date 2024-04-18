544616d220bbae69fd09cd6a41b2bb2f1e066390
Jakub Cieślik <kuba8172@gmail.com>
2024-04-18 23:50:07 +0200

    UPDATE: disable scroll when modal is open

--- frontend/src/components/ui/Modal.tsx
+++ frontend/src/components/ui/Modal.tsx
@@ -1,5 +1,5 @@
 import { useTransition, animated, useSpring } from "@react-spring/web";
-import React, { Fragment } from "react";
+import React, { Fragment, useEffect } from "react";
 
 export type ModalProps = {
   children?: React.ReactNode;
@@ -17,13 +17,22 @@ function Modal({ children, isShow, setIsShow }: ModalProps) {
   const backgroundTranstion = useSpring({
     opacity: isShow ? 1 : 0,
   });
+
+  useEffect(() => {
+    if (isShow) {
+      document.body.style.overflow = "hidden";
+    } else {
+      document.body.style.overflow = "auto";
+    }
+  }, [isShow]);
+
   return transitions(
     (style, item) =>
       item && (
         <div className="absolute top-0 left-0 w-screen h-screen flex items-center justify-center">
           <animated.div
             style={style}
-            className="z-10 container px-4 py-5 flex flex-col gap-5 items-center w-[90%] lg:w-[40%] h-[90%] rounded-lg border border-white/70   bg-dashboardPrimary"
+            className="z-10 container px-4 py-5 flex flex-col gap-5 items-center w-[90%] lg:w-[40%] h-[95%] rounded-lg border border-white/70 bg-dashboardPrimary overflow-x-auto"
           >
             {children}
           </animated.div>
