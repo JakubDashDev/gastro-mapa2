d6e303e4bd37ecfb24b50ad116eeb8d5ae917281
Jakub Cieślik <kuba8172@gmail.com>
2024-04-20 22:14:37 +0200

    FIX: typo fix

--- frontend/src/components/ui/MapMarker.tsx
+++ frontend/src/components/ui/MapMarker.tsx
@@ -4,22 +4,21 @@ import img from "../../assets/logo.png";
 import MarkerPopup from "./MarkerPopup";
 import useWindowDimensions from "../../hooks/useWindoDimensions";
 import MobileMarkerPopup from "./MobileMarkerPopup";
-import { Restaurant } from "../../redux/restaurantsSlice";
+import { RestaurantType } from "../../redux/restaurantsSlice";
 
 type MapMarkerProps = {
-  data: Restaurant[];
+  data: RestaurantType[];
 };
 
 function MapMarker({ data }: MapMarkerProps) {
-  const [popupInfo, setPopupInfo] = useState<Restaurant | null>(null);
+  const [popupInfo, setPopupInfo] = useState<RestaurantType | null>(null);
   const { width } = useWindowDimensions();
-
 
   const markers = data.map((restaurant, index) => (
     <Marker
       key={index}
-      longitude={restaurant.address.latLng[0]}
-      latitude={restaurant.address.latLng[1]}
+      longitude={restaurant.address.lngLat[0]}
+      latitude={restaurant.address.lngLat[1]}
       onClick={(e) => {
         // If we let the click event propagates to the map, it will immediately close the popup
         // with `closeOnClick: true`
@@ -27,25 +26,15 @@ function MapMarker({ data }: MapMarkerProps) {
         setPopupInfo(restaurant);
       }}
     >
-      <img
-        src={img}
-        alt="marker"
-        width={50}
-        height={50}
-        className="cursor-pointer"
-      />
+      <img src={img} alt="marker" width={50} height={50} className="cursor-pointer" />
     </Marker>
   ));
 
   return (
     <>
       {markers}
-      {popupInfo && width >= 640 && (
-        <MarkerPopup popupInfo={popupInfo} setPopupInfo={setPopupInfo} />
-      )}
-      {width < 640 && (
-        <MobileMarkerPopup popupInfo={popupInfo} setPopupInfo={setPopupInfo} />
-      )}
+      {popupInfo && width >= 640 && <MarkerPopup popupInfo={popupInfo} setPopupInfo={setPopupInfo} />}
+      {width < 640 && <MobileMarkerPopup popupInfo={popupInfo} setPopupInfo={setPopupInfo} />}
     </>
   );
 }
