c61c0cddec17602284164fb74d830537613a51c9
Jakub Cieślik <kuba8172@gmail.com>
2024-04-20 22:19:07 +0200

    UPDATE: styles

--- frontend/src/components/ui/NavBar.tsx
+++ frontend/src/components/ui/NavBar.tsx
@@ -12,24 +12,23 @@ type NavBarProps = {
 function NavBar({ setShowSidebar }: NavBarProps) {
   const { userInfo } = useAppSelector((state) => state.auth);
   return (
-    <nav className="container mx-auto px-4 flex items-center justify-between w-full py-5 h-[100px]">
-      <div className="flex items-center gap-5 lg:gap-10">
-        <button
-          className="block lg:hidden"
-          onClick={() => setShowSidebar((current) => !current)}
-        >
-          <FaBars />
-        </button>
-        <Breadcrumbs />
-      </div>
-      <div className="flex gap-5 text-gray-600 text-lg">
-        <button>
-          <FaBell />
-        </button>
-        <Link to="/dashboard/account" className="flex items-center gap-1">
-          <FaUser />
-          <span className="hidden sm:block text-base">{userInfo.email}</span>
-        </Link>
+    <nav className="bg-neutral-50 border-b-2 border-gray-300  w-full py-5 h-[100px]">
+      <div className="container mx-auto px-4 flex items-center justify-between">
+        <div className=" flex items-center gap-5 lg:gap-10">
+          <button className="block lg:hidden" onClick={() => setShowSidebar((current) => !current)}>
+            <FaBars />
+          </button>
+          <Breadcrumbs />
+        </div>
+        <div className="flex gap-5 text-gray-600 text-lg">
+          <button>
+            <FaBell />
+          </button>
+          <Link to="/dashboard/account" className="flex items-center gap-1">
+            <FaUser />
+            <span className="hidden sm:block text-base">{userInfo.email}</span>
+          </Link>
+        </div>
       </div>
     </nav>
   );
