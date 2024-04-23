c0efdc27de1f99294c01b714fb7002333fe999e4
Jakub Cieślik <kuba8172@gmail.com>
2024-04-23 12:35:07 +0200

    UPDATE: styles & NEW: message when restaurants array.length equals 0

--- frontend/src/components/ui/SideNav.tsx
+++ frontend/src/components/ui/SideNav.tsx
@@ -18,53 +18,36 @@ type SideNavProps = {
 function SideNav({ data, darkMode, setDarkMode }: SideNavProps) {
   const { width } = useWindowDimensions();
 
-  if (width >= 1280)
-    return (
-      <DesktopSideNav
-        data={data}
-        darkMode={darkMode}
-        setDarkMode={setDarkMode}
-      />
-    );
-  if (width < 1280)
-    return (
-      <MobileSideNav
-        data={data}
-        darkMode={darkMode}
-        setDarkMode={setDarkMode}
-      />
-    );
+  if (width >= 1280) return <DesktopSideNav data={data} darkMode={darkMode} setDarkMode={setDarkMode} />;
+  if (width < 1280) return <MobileSideNav data={data} darkMode={darkMode} setDarkMode={setDarkMode} />;
 }
 
 export default SideNav;
 
 function DesktopSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
   return (
-    <div className="w-[390px] bg-white dark:bg-darkBg h-screen overflow-x-auto relative text-gray-600 dark:text-darkText">
-      <h1 className="text-center text-lg font-extrabold my-5 col-span-2">
-        Gastro Mapa
-      </h1>
+    <div className="w-[390px] flex flex-col gap-3 bg-white dark:bg-darkBg h-screen overflow-x-auto relative text-gray-600 dark:text-darkText">
+      <h1 className="text-center text-lg font-extrabold my-5 col-span-2">Gastro Mapa</h1>
 
-      <div className="container mx-auto px-4 my-6">
+      <div className="container mx-auto px-4 ">
         <Search />
       </div>
-      <div className="container mx-auto px-4 my-6 flex justify-between items-center">
+      <div className="container mx-auto px-4  flex justify-between items-center">
         <SortDropdown />
         <FilterButton />
       </div>
 
-      <div>
-        {data.map((restaurant) => {
-          return (
-            <RestaurantListComponent
-              key={restaurant._id}
-              restaurant={restaurant}
-            />
-          );
-        })}
-      </div>
+      {data.length === 0 ? (
+        <div className="flex flex-col flex-grow text-gray-400 dark:text-gray-500 text-3xl font-bold px-4 justify-center ">
+          <span>Nie ma restauracji o podanych kryteriach :(</span>
+        </div>
+      ) : (
+        data.map((restaurant: RestaurantType) => {
+          return <RestaurantListComponent key={restaurant._id} restaurant={restaurant} />;
+        })
+      )}
 
-      <div className="absolute bottom-0 p-4 bg-white dark:bg-darkBg w-full border-t border-black/10 dark:border-white/10">
+      <div className="p-4 bg-white dark:bg-darkBg w-full border-t border-black/10 dark:border-white/10">
         <DarkModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
     </div>
@@ -102,16 +85,10 @@ function MobileSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
                 className="w-screen sm:w-[390px] h-screen absolute top-0 left-0 bg-white dark:bg-darkBg text-gray-600 dark:text-darkText z-30"
               >
                 <div className="grid items-center grid-rows-1 grid-cols-4 container mx-auto px-4">
-                  <button
-                    type="button"
-                    className="text-lg"
-                    onClick={() => setIsOpen((current) => !current)}
-                  >
+                  <button type="button" className="text-lg" onClick={() => setIsOpen((current) => !current)}>
                     <FaArrowLeft />
                   </button>
-                  <h1 className="text-center text-lg font-extrabold my-5 col-span-2">
-                    Gastro Mapa
-                  </h1>
+                  <h1 className="text-center text-lg font-extrabold my-5 col-span-2">Gastro Mapa</h1>
                 </div>
 
                 <div className="container mx-auto px-4 my-6">
@@ -125,18 +102,12 @@ function MobileSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
                 <div>
                   {data.map((restaurant) => {
                     return (
-                      <RestaurantListComponent
-                        key={restaurant._id}
-                        restaurant={restaurant}
-                      />
+                      <RestaurantListComponent key={restaurant._id} restaurant={restaurant} handleClose={setIsOpen} />
                     );
                   })}
                 </div>
-                <div className="absolute bottom-0 p-4 bg-white dark:bg-darkBg w-full border-t border-black/20 dark:border-white/20">
-                  <DarkModeButton
-                    darkMode={darkMode}
-                    setDarkMode={setDarkMode}
-                  />
+                <div className="mt-auto p-4 bg-white dark:bg-darkBg w-full border-t border-black/20 dark:border-white/20">
+                  <DarkModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
                 </div>
               </animated.nav>
               <animated.div
@@ -163,19 +134,12 @@ function DarkModeButton({ darkMode, setDarkMode }: DarkModeButtonProps) {
   };
 
   return (
-    <button
-      className={`toggle-btn ${darkMode ? "toggled" : undefined}`}
-      onClick={toggle}
-    >
+    <button className={`toggle-btn ${darkMode ? "toggled" : undefined}`} onClick={toggle}>
       <div className="thumb"></div>
       {darkMode ? (
-        <span className="absolute left-1 text-xs bottom-[50%] translate-y-[50%] text-white">
-          Dark
-        </span>
+        <span className="absolute left-1 text-xs bottom-[50%] translate-y-[50%] text-white">Dark</span>
       ) : (
-        <span className="absolute right-1 text-xs bottom-[50%] translate-y-[50%] text-black">
-          Light
-        </span>
+        <span className="absolute right-1 text-xs bottom-[50%] translate-y-[50%] text-black">Light</span>
       )}
     </button>
   );
