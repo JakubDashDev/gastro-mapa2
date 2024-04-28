adb85b7f44d73de3ee8fbe2b3553dd29d4b85ff7
Jakub Cieślik <kuba8172@gmail.com>
2024-04-28 22:46:15 +0200

    NEW: passwordConfirm validation

--- backend/src/models/AdminUserModel.ts
+++ backend/src/models/AdminUserModel.ts
@@ -1,30 +1,64 @@
 import mongoose, { Schema } from "mongoose";
 import bcrypt from "bcryptjs";
 import { Document } from "mongodb";
+import crypto from "crypto";
 
 export interface IUser extends Document {
   username: string;
   email: string;
   password: string;
+  passwordConfirm: string | undefined;
+  passwordChangeAt: number;
 }
 
-const adminUserSchema: Schema<IUser> = new mongoose.Schema({
-  username: { type: String, require: true },
-  email: { type: String, require: true, lowercase: true },
-  password: { type: String, require: true, minLength: 8, select: false },
+const adminUserSchema = new mongoose.Schema<IUser>({
+  username: { type: String, required: true },
+  email: { type: String, required: true, lowercase: true },
+  password: { type: String, required: true, minLength: 8, select: false },
+  passwordConfirm: {
+    type: String,
+    required: [true, "Proszę, potwierdź swoje hasło"],
+  },
+  passwordChangeAt: Date,
+});
+adminUserSchema.pre("validate", async function (next) {
+  if (this.password !== this.passwordConfirm) {
+    this.invalidate("passwordConfirm", "Hasła nie są identyczne!");
+  }
+
+  next();
+});
+
+adminUserSchema.pre("save", async function (next) {
+  if (!this.isModified("password") || this.isNew) return next();
+
+  this.passwordChangeAt = Date.now() - 1000;
+
+  next();
 });
 
 adminUserSchema.pre("save", async function (next) {
   if (!this.isModified("password")) return next();
 
   this.password = await bcrypt.hash(this.password, 12);
+
+  this.passwordConfirm = undefined;
   next();
 });
 
-adminUserSchema.methods.matchPassword = async function (
-  enteredPassword: string
-) {
+adminUserSchema.methods.matchPassword = async function (enteredPassword: string) {
   return await bcrypt.compare(enteredPassword, this.password);
+};
+
+adminUserSchema.methods.createPasswordResetToken = async function () {
+  const resetToken = crypto.randomBytes(32).toString("hex");
+
+  this.passwordResetToken = crypto.createHash("sha256").update(resetToken).digest("hex");
+  this.passwordResetExpires = Date.now() + 10 * 60 * 1000; //10 minutes
+
+  console.log({ resetToken }, this.passwordResetToken);
+
+  return resetToken;
 };
 
 const AdminUser = mongoose.model("AdminUser", adminUserSchema);
