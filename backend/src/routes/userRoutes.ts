e53327df11dc0f572b8dd7e72fcb09ee14160df5
Jakub Cieślik <kuba8172@gmail.com>
2024-04-28 22:46:38 +0200

    NEW: updatePassword route

--- backend/src/routes/userRoutes.ts
+++ backend/src/routes/userRoutes.ts
@@ -1,9 +1,5 @@
 import express from "express";
-import {
-  authAdmin,
-  createAdmin,
-  logout,
-} from "../controllers/authController.js";
+import { authAdmin, createAdmin, logout, updatePassword } from "../controllers/authController.js";
 import { getAllRestaurants } from "../controllers/restaurantController.js";
 import { protect } from "../middleware/authMiddleware.js";
 
@@ -12,5 +8,6 @@ const router = express.Router();
 router.route("/auth").post(authAdmin);
 router.route("/").post(createAdmin);
 router.route("/logout").post(logout);
+router.route("/updatePassword").patch(protect, updatePassword);
 
 export default router;
