368f73a5d67c34b57a4cd37c220a19b211f52908
Jakub Cieślik <kuba8172@gmail.com>
2024-05-09 21:16:25 +0200

    UPDATE: reponsive design and scollbar

--- frontend/src/components/ui/SideNav.tsx
+++ frontend/src/components/ui/SideNav.tsx
@@ -5,7 +5,7 @@ import SortDropdown from "../features/SortDropdown";
 import FilterButton from "../features/FilterButton";
 import useWindowDimensions from "../../hooks/useWindoDimensions";
 import { MdMenuOpen } from "react-icons/md";
-import { FaArrowLeft } from "react-icons/fa";
+import { FaTimes } from "react-icons/fa";
 import { useSpring, animated, useTransition } from "@react-spring/web";
 import { RestaurantType } from "../../redux/restaurantsSlice";
 
@@ -16,18 +16,20 @@ type SideNavProps = {
 };
 
 function SideNav({ data, darkMode, setDarkMode }: SideNavProps) {
-  const { width } = useWindowDimensions();
+  const [showSidebar, setShowSidebar] = useState(false);
 
-  if (width >= 1280) return <DesktopSideNav data={data} darkMode={darkMode} setDarkMode={setDarkMode} />;
-  if (width < 1280) return <MobileSideNav data={data} darkMode={darkMode} setDarkMode={setDarkMode} />;
-}
-
-export default SideNav;
-
-function DesktopSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
   return (
-    <div className="w-[390px] flex flex-col gap-3 bg-white dark:bg-darkBg h-screen overflow-x-auto relative text-gray-600 dark:text-darkText">
-      <h1 className="text-center text-lg font-extrabold my-5 col-span-2">Gastro Mapa</h1>
+    <Container showSidebar={showSidebar} setShowSidebar={setShowSidebar}>
+      <div>
+        <h1 className="text-center text-xl font-extrabold my-5 col-span-2">Gastro Mapa</h1>
+        <button
+          type="button"
+          className="block lg:hidden text-gray-600 dark:text-white/80 text-xl absolute top-2 right-2"
+          onClick={() => setShowSidebar((current) => !current)}
+        >
+          <FaTimes />
+        </button>
+      </div>
 
       <div className="container mx-auto px-4 ">
         <Search />
@@ -37,88 +39,80 @@ function DesktopSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
         <FilterButton />
       </div>
 
-      {data.length === 0 ? (
-        <div className="flex flex-col flex-grow text-gray-400 dark:text-gray-500 text-3xl font-bold px-4 justify-center ">
-          <span>Nie ma restauracji o podanych kryteriach :(</span>
-        </div>
-      ) : (
-        data.map((restaurant: RestaurantType) => {
-          return <RestaurantListComponent key={restaurant._id} restaurant={restaurant} />;
-        })
-      )}
+      <div id="restaurantListSideNav" className="overflow-x-auto">
+        {data.length === 0 ? (
+          <div className="flex flex-col flex-grow text-gray-400 dark:text-gray-500 text-3xl font-bold px-4 justify-center ">
+            <span>Nie ma restauracji o podanych kryteriach 💔</span>
+          </div>
+        ) : (
+          data.map((restaurant: RestaurantType) => {
+            return <RestaurantListComponent key={restaurant._id} restaurant={restaurant} />;
+          })
+        )}
+      </div>
 
       <div className="p-4 bg-white dark:bg-darkBg w-full border-t border-black/10 dark:border-white/10">
         <DarkModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
       </div>
-    </div>
+    </Container>
   );
 }
 
-function MobileSideNav({ data, darkMode, setDarkMode }: SideNavProps) {
-  const [isOpen, setIsOpen] = useState(false);
+export default SideNav;
 
-  const transition = useTransition(isOpen, {
+type ContainerProps = {
+  children: React.ReactNode;
+  showSidebar: boolean;
+  setShowSidebar: React.Dispatch<React.SetStateAction<boolean>>;
+};
+function Container({ children, showSidebar, setShowSidebar }: ContainerProps) {
+  const { width } = useWindowDimensions();
+
+  const transition = useTransition(showSidebar, {
     from: { transform: "translateX(-100%)" },
     enter: { transform: "translateX(0%)" },
     leave: { transform: "translateX(-100%)" },
   });
 
   const backdropAnimation = useSpring({
-    opacity: isOpen ? 1 : 0,
+    opacity: showSidebar ? 1 : 0,
   });
 
+  if (width < 1024)
+    return (
+      <Fragment>
+        <button
+          type="button"
+          className="absolute top-5 left-5 bg-white border-2 border-[#d3d3d3] p-2 rounded-full shadow-xl shadow-black/60 z-10 text-2xl"
+          onClick={() => setShowSidebar((current) => !current)}
+        >
+          <MdMenuOpen />
+        </button>
+        {transition(
+          (styles, item) =>
+            item && (
+              <Fragment>
+                <animated.aside
+                  style={styles}
+                  className="fixed left-0 h-screen w-screen md:w-[420px] z-20 flex flex-col gap-3 bg-white dark:bg-darkBg text-gray-600 dark:text-darkText"
+                >
+                  {children}
+                </animated.aside>
+                <animated.div
+                  style={backdropAnimation}
+                  className="fixed left-0 top-0 w-screen h-screen bg-black/20 z-10"
+                  onClick={() => setShowSidebar(false)}
+                />
+              </Fragment>
+            )
+        )}
+      </Fragment>
+    );
+
   return (
-    <Fragment>
-      <button
-        type="button"
-        className="absolute top-5 left-5 bg-white border-2 border-[#d3d3d3] p-2 rounded-full shadow-xl shadow-black/60 z-10 text-2xl"
-        onClick={() => setIsOpen((current) => !current)}
-      >
-        <MdMenuOpen />
-      </button>
-      {transition(
-        (styles, item) =>
-          item && (
-            <Fragment>
-              <animated.nav
-                style={styles}
-                className="w-screen sm:w-[390px] h-screen absolute top-0 left-0 bg-white dark:bg-darkBg text-gray-600 dark:text-darkText z-30"
-              >
-                <div className="grid items-center grid-rows-1 grid-cols-4 container mx-auto px-4">
-                  <button type="button" className="text-lg" onClick={() => setIsOpen((current) => !current)}>
-                    <FaArrowLeft />
-                  </button>
-                  <h1 className="text-center text-lg font-extrabold my-5 col-span-2">Gastro Mapa</h1>
-                </div>
-
-                <div className="container mx-auto px-4 my-6">
-                  <Search />
-                </div>
-                <div className="container mx-auto px-4 my-6 flex justify-between items-center">
-                  <SortDropdown />
-                  <FilterButton />
-                </div>
-
-                <div>
-                  {data.map((restaurant) => {
-                    return (
-                      <RestaurantListComponent key={restaurant._id} restaurant={restaurant} handleClose={setIsOpen} />
-                    );
-                  })}
-                </div>
-                <div className="mt-auto p-4 bg-white dark:bg-darkBg w-full border-t border-black/20 dark:border-white/20">
-                  <DarkModeButton darkMode={darkMode} setDarkMode={setDarkMode} />
-                </div>
-              </animated.nav>
-              <animated.div
-                style={backdropAnimation}
-                className="absolute top-0 left-0 w-screen h-screen bg-black/30 z-20 "
-                onClick={() => setIsOpen((current) => !current)}
-              />
-            </Fragment>
-          )
-      )}
-    </Fragment>
+    <aside className="w-[420px] flex flex-col gap-3 bg-white dark:bg-darkBg h-screen overflow-x-auto relative text-gray-600 dark:text-darkText">
+      {children}
+    </aside>
   );
 }
 
