67de08064cac821003c4c3aed6bdb62bc1e45868
Jakub Cieślik <kuba8172@gmail.com>
2024-05-09 21:16:37 +0200

    UPDATE: added new prop

--- frontend/src/components/ui/Loader.tsx
+++ frontend/src/components/ui/Loader.tsx
@@ -2,10 +2,11 @@ import React from "react";
 
 type LoaderType = {
   color?: string;
+  colorDark?: string;
 };
 
-function Loader({ color }: LoaderType) {
-  return <span id="loader" className={`border-[5px] border-${color}`}></span>;
+function Loader({ color, colorDark }: LoaderType) {
+  return <span id="loader" className={`border-[4px] border-${color} dark:border-${colorDark}`}></span>;
 }
 
 export default Loader;
