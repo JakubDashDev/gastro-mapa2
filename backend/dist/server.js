660e81c2e09c9c44ab1bc29a8d03f0872da0da39
Jakub Cieślik <kuba8172@gmail.com>
2024-05-12 00:59:56 +0200

    production

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -48,7 +48,7 @@ app.use("/api/admin", userRoutes_js_1.default);
 if (process.env.NODE_ENV === "prod") {
     const __dirname = path_1.default.resolve();
     app.use(express_1.default.static(path_1.default.join(__dirname, "../../frontend/build")));
-    app.get("*", (req, res) => res.sendFile(path_1.default.resolve(__dirname, "..", "..", "frontend", "build", "index.html")));
+    app.get("*", (req, res) => res.sendFile(path_1.default.resolve(__dirname, "..", "..", "frontend", "dist", "index.html")));
 }
 //Error middleware
 app.use(errorMiddleware_js_1.errorHandler);
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -57,7 +57,7 @@ if (process.env.NODE_ENV === "prod") {
   const __dirname = path.resolve();
 
   app.use(express.static(path.join(__dirname, "../../frontend/build")));
-  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "..", "frontend", "build", "index.html")));
+  app.get("*", (req, res) => res.sendFile(path.resolve(__dirname, "..", "..", "frontend", "dist", "index.html")));
 }
 
 //Error middleware
