a4d27099938f7228bfbefb0190a0d95c164af938
Jakub Cieślik <kuba8172@gmail.com>
2024-05-12 02:36:11 +0200

    production

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -15,16 +15,7 @@ import path from "path";
 const app = express();
 
 //Security HTTP headers
-app.use(
-  helmet({
-    contentSecurityPolicy: {
-      directives: {
-        defaultSrc: ["'self'"],
-        connectSrc: ["'self'", "http://127.0.0.1:3001", "ws://localhost:42877/"],
-      },
-    },
-  })
-);
+app.use(helmet());
 
 //Body parser
 app.use(express.json({ limit: "10kb" }));
