be6cfbe47bb6e1bb0e8ff6154a46558957a5df57
Jakub Cieślik <kuba8172@gmail.com>
2024-05-12 02:49:33 +0200

    production

--- backend/dist/server.js
+++ backend/dist/server.js
@@ -18,14 +18,35 @@ const express_mongo_sanitize_1 = __importDefault(require("express-mongo-sanitize
 const path_1 = __importDefault(require("path"));
 const app = (0, express_1.default)();
 //Security HTTP headers
-app.use((0, helmet_1.default)());
+const scriptSrcUrls = ["https://api.tiles.mapbox.com/", "https://api.mapbox.com/"];
+const styleSrcUrls = ["https://api.mapbox.com/", "https://api.tiles.mapbox.com/", "https://fonts.googleapis.com/"];
+const connectSrcUrls = [
+    "https://api.mapbox.com/",
+    "https://a.tiles.mapbox.com/",
+    "https://b.tiles.mapbox.com/",
+    "https://events.mapbox.com/",
+];
+const fontSrcUrls = ["fonts.googleapis.com", "fonts.gstatic.com"];
+app.use((0, helmet_1.default)({
+    contentSecurityPolicy: {
+        directives: {
+            defaultSrc: [],
+            connectSrc: ["'self'", ...connectSrcUrls],
+            scriptSrc: ["'self'", ...scriptSrcUrls],
+            styleSrc: ["'self'", "'unsafe-inline'", ...styleSrcUrls],
+            workerSrc: ["'self'", "blob:"],
+            objectSrc: [],
+            imgSrc: ["'self'", "blob:", "data:"],
+            fontSrc: ["'self'", ...fontSrcUrls],
+        },
+    },
+}));
 //Body parser
 app.use(express_1.default.json({ limit: "10kb" }));
 //Data sanitization
 app.use((0, express_mongo_sanitize_1.default)());
 //CORS
 app.use((0, cors_1.default)({
-    origin: "https://gastro-mapa2-frontend.onrender.com",
     credentials: true,
 }));
 //COOKIES
