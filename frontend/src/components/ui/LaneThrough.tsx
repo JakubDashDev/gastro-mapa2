567b0b23649cbcdb075bbb3e53f8c1d395c60309
Jakub Cieślik <kuba8172@gmail.com>
2024-06-11 11:42:48 +0200

    UPDATE: UI update

--- frontend/src/components/ui/LaneThrough.tsx
+++ frontend/src/components/ui/LaneThrough.tsx
@@ -2,7 +2,7 @@ function LaneThrough({ title }: { title: string }) {
   return (
     <div className="relative flex py-2 items-center -mb-5">
       <div className="flex-grow border-t border-gray-400"></div>
-      <span className="flex-shrink mx-4 text-gray-400">{title}</span>
+      <span className="flex-shrink mx-4 text-gray-600">{title}</span>
       <div className="flex-grow border-t border-gray-400"></div>
     </div>
   );
