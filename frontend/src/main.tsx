d70275b5193b35a8b5eeb1622e9a3a33157bf1cb
Jakub Cieślik <kuba8172@gmail.com>
2024-06-11 11:10:43 +0200

    UPDATE: query added to main route & NEW: errorElement

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -5,18 +5,21 @@ import "./index.css";
 import { Provider } from "react-redux";
 import store from "./redux/store";
 import { MapProvider } from "react-map-gl";
-import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import { Link, RouterProvider, createBrowserRouter } from "react-router-dom";
 import LoginPage from "./pages/admin/LoginPage";
 import HomePage from "./pages/admin/HomePage";
 import RestaurantsPage from "./pages/admin/RestaurantsListPage";
 import UserAccountSettingsPage from "./pages/admin/UserAccountSettingsPage";
 import ReactErrorBoundary from "./components/ReactErrorBoundary";
+import RouteError from "./components/RouteError";
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: "/:query?",
     element: <App />,
+    errorElement: <RouteError />,
   },
+
   {
     path: "/dashboard",
     async lazy() {
@@ -27,28 +30,43 @@ const router = createBrowserRouter([
       {
         path: "",
         element: <HomePage />,
+        errorElement: <RouteError />,
       },
       {
         path: "/dashboard/restaurants/:id?",
         element: <RestaurantsPage />,
+        errorElement: <RouteError />,
       },
       {
         path: "/dashboard/users",
         element: <span className="mt-5 w-full flex items-center justify-center text-3xl">Strona w budowie 🛠️</span>,
+        errorElement: <RouteError />,
       },
       {
         path: "/dashboard/stats",
         element: <span className="mt-5 w-full flex items-center justify-center text-3xl">Strona w budowie 🛠️</span>,
+        errorElement: <RouteError />,
       },
       {
         path: "/dashboard/account",
         element: <UserAccountSettingsPage />,
+        errorElement: <RouteError />,
       },
     ],
   },
   {
     path: "/dashboard/auth",
     element: <LoginPage />,
+    errorElement: <RouteError />,
+  },
+  {
+    path: "*", //404 error
+    element: (
+      <div className="flex-col gap-2 w-screen h-[calc(100dvh)] bg-darkBg flex items-center justify-center text-white">
+        <span>Chyba się zgubiłeś 🤔</span>
+        <Link to="/" className="text-blue-500 underline underline-offset-4">Wróc do strony głównej</Link>
+      </div>
+    ),
   },
 ]);
 
