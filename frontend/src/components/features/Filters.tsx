ad1be56bcea174f28b9903e1929cf4c83c72dd3f
Jakub Cieślik <kuba8172@gmail.com>
2024-06-11 11:34:58 +0200

    UPDATE: filters

--- frontend/src/components/features/Filters.tsx
+++ frontend/src/components/features/Filters.tsx
@@ -1,29 +1,41 @@
 import React, { useEffect, useState } from "react";
 import { RATING_ARRAY, CATEGORY_ARRAY } from "../../../constatns";
 import Rating from "../ui/Rating";
-import { clearFilterQuery, setIsActive, updateFilterQuery } from "../../redux/filtersSlice";
 import useGetRestaurantsLazy from "../../hooks/useGetRestaurantsLazy";
 import { useTransition, animated } from "@react-spring/web";
-import { useAppDispatch, useAppSelector } from "../../redux/store";
+import { useAppDispatch } from "../../redux/store";
 import PromiseButton from "../ui/PromiseButton";
 import Loader from "../ui/Loader";
+import { useNavigate } from "react-router-dom";
+import setParams from "../../utils/setUrlParams";
+import deleteUrlParams from "../../utils/deleteUrlParams";
+import getParams from "../../utils/getUrlParams";
 
 type FiltersProps = {
   setIsOpen: React.Dispatch<React.SetStateAction<boolean>>;
 };
 function Filters({ setIsOpen }: FiltersProps) {
-  const dispatch = useAppDispatch();
-  const { filterQuery } = useAppSelector((state) => state.filters);
+  const navigate = useNavigate();
+
+  const { filtersQuery } = getParams(location);
+  const filters = filtersQuery && JSON.parse(filtersQuery);
+
   const { getRestaurants, isLoading, error, isError, isSuccess, isFetching } = useGetRestaurantsLazy();
+
+  const [filtersState, setFiltersState] = useState<any>(filters || []);
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
-    getRestaurants({
-      keyword: undefined,
-      filters: JSON.stringify(filterQuery),
-    });
 
-    filterQuery.length > 0 ? dispatch(setIsActive(true)) : dispatch(setIsActive(false));
+    if (filtersState.length > 0) {
+      const params = setParams("filters", JSON.stringify(filtersState));
+      navigate({ search: params });
+    } else {
+      const params = deleteUrlParams("filters");
+      navigate({ search: params });
+    }
+
+    getRestaurants(location.search);
   };
 
   useEffect(() => {
@@ -33,18 +45,17 @@ function Filters({ setIsOpen }: FiltersProps) {
   }, [isSuccess, isLoading, isError, isFetching]);
 
   const handleClear = () => {
-    dispatch(clearFilterQuery());
-    getRestaurants({
-      keyword: undefined,
-      filters: undefined,
-    });
+    const params = deleteUrlParams("filters");
+
+    navigate({ search: params });
+    getRestaurants(location.search);
   };
 
   return (
     <form onSubmit={handleSubmit}>
-      <RatingFilterSection isLoading={isLoading} />
+      <RatingFilterSection isLoading={isLoading} setFiltersState={setFiltersState} filtersState={filtersState} />
 
-      <CategoryFilterSection isLoading={isLoading} />
+      <CategoryFilterSection isLoading={isLoading} setFiltersState={setFiltersState} filtersState={filtersState} />
 
       {isError && (
         <div className="w-full border border-red-500 my-3 p-2 rounded-lg bg-red-900/40">
@@ -53,20 +64,18 @@ function Filters({ setIsOpen }: FiltersProps) {
       )}
       <div className="flex flex-row gap-3 w-full mt-5">
         <PromiseButton
-          isLoading={isFetching}
-          isError={isError}
-          isSuccess={isError}
           disabled={isLoading}
           type="submit"
-          bgColor="primary-500"
+          status={isFetching ? "loading" : isError ? "error" : null}
+          className="w-full bg-primary-500 hover:bg-primary-400 rounded-md"
         >
           Zastosuj
         </PromiseButton>
         <button
           type="button"
-          className="w-full flex items-center justify-center sm:w-1/2 py-1 rounded-md border border-gray-700 transition-colors dark:disabled:text-gray-700 disabled:text-gray-300"
+          className="w-full flex items-center justify-center sm:w-1/2 py-1 rounded-md border border-gray-500 disabled:border-gray-300 disabled:dark:border-gray-700 transition-colors dark:disabled:text-gray-700 disabled:text-gray-300"
           onClick={handleClear}
-          disabled={filterQuery.length === 0 || isLoading}
+          disabled={filtersState.length === 0 || isLoading}
         >
           {isFetching ? <Loader color="gray-500" colorDark="gray-200" /> : "Wyczyść"}
         </button>
@@ -79,23 +88,41 @@ export default Filters;
 
 interface FilterButtonType extends React.ButtonHTMLAttributes<HTMLButtonElement> {
   value: string | number;
+  setFilertsState: React.Dispatch<any>;
+  filtersState: any;
 }
 
-function FilterButton({ value, disabled }: FilterButtonType) {
-  const dispatch = useAppDispatch();
-  const { filterQuery } = useAppSelector((state) => state.filters);
-
-  const [checked, setChecked] = useState(false);
+function FilterButton({ value, setFilertsState, filtersState, disabled }: FilterButtonType) {
+  const { filtersQuery } = getParams(location);
+  const filters = filtersQuery && JSON.parse(filtersQuery);
+  const initialValue = filters ? filters.some((item: any) => item.$gte === value || item.category === value) : false;
+  const [checked, setChecked] = useState<boolean>(initialValue);
 
   const handleClick = () => {
-    dispatch(updateFilterQuery(value));
+    const condition = filtersState.some((item: any) => item.$gte === value || item.category === value);
+
+    if (RATING_ARRAY.includes(value) && !condition) {
+      //NOTE: handling custom value of rating
+      if (value === "challange ostrości") {
+        setFilertsState((current: any) => [...current, { $gte: value, $lte: value }]);
+      } else {
+        setFilertsState((current: any) => [...current, { $gte: value, $lte: (value as number) + 0.9 }]);
+      }
+    }
+
+    if (RATING_ARRAY.includes(value) && condition) {
+      setFilertsState((current: any) => current.filter((item: any) => item.$gte !== value));
+    }
+
+    if (CATEGORY_ARRAY.includes(value as string) && !condition) {
+      setFilertsState((current: any) => [...current, { category: value }]);
+    }
+
+    if (CATEGORY_ARRAY.includes(value as string) && condition) {
+      setFilertsState((current: any) => current.filter((item: any) => item.category !== value));
+    }
+    setChecked((current) => !current);
   };
-
-  useEffect(() => {
-    const condition = filterQuery.some((item) => item.$gte === value || item.category === value);
-
-    setChecked(condition);
-  }, [filterQuery]);
 
   return (
     <label
@@ -108,9 +135,9 @@ function FilterButton({ value, disabled }: FilterButtonType) {
         id={value.toString()}
         value={value}
         className="scale-125"
-        onChange={handleClick}
         disabled={disabled}
         checked={checked}
+        onChange={handleClick}
       />
       {RATING_ARRAY.includes(value) ? <Rating rating={value} color="text-gray-600 dark:text-gray-200" /> : value}
     </label>
@@ -119,19 +146,19 @@ function FilterButton({ value, disabled }: FilterButtonType) {
 
 type FilterSectionProps = {
   isLoading: boolean;
+  setFiltersState: React.Dispatch<any>;
+  filtersState: any;
 };
 
-function RatingFilterSection({ isLoading }: FilterSectionProps) {
+function RatingFilterSection({ isLoading, setFiltersState, filtersState }: FilterSectionProps) {
   const [isShow, setIsShow] = useState(false);
-
-  const { filterQuery } = useAppSelector((state) => state.filters);
-
-  const ratingQuery = filterQuery.filter((item) => item.hasOwnProperty("$gte"));
+  const { filtersQuery } = getParams(location);
+  const ratingFilters = filtersQuery && JSON.parse(filtersQuery).filter((item: any) => item.hasOwnProperty("$gte"));
 
   const transition = useTransition(isShow, {
     from: { height: "0px", opacity: 0, marginBottom: "0px", marginTop: "0px" },
     enter: {
-      height: "175px",
+      height: "185px",
       opacity: 1,
       marginBottom: "20px",
       marginTop: "10px",
@@ -144,7 +171,7 @@ function RatingFilterSection({ isLoading }: FilterSectionProps) {
       <div className="flex items-center justify-between w-full border-b text-lg">
         <div className="flex gap-1">
           <span>Ocena</span>
-          {ratingQuery.length > 0 && <span className="text-red-500 text-sm">({ratingQuery.length})</span>}
+          {ratingFilters.length > 0 && <span className="text-red-500 text-sm">({ratingFilters.length})</span>}
         </div>
         <button
           type="button"
@@ -160,7 +187,13 @@ function RatingFilterSection({ isLoading }: FilterSectionProps) {
             <div className="overflow-hidden">
               <animated.div style={styles} className="flex flex-col gap-1 px-1">
                 {RATING_ARRAY.map((rating) => (
-                  <FilterButton key={rating} value={rating} disabled={isLoading} />
+                  <FilterButton
+                    key={rating}
+                    value={rating}
+                    disabled={isLoading}
+                    setFilertsState={setFiltersState}
+                    filtersState={filtersState}
+                  />
                 ))}
               </animated.div>
             </div>
@@ -170,16 +203,16 @@ function RatingFilterSection({ isLoading }: FilterSectionProps) {
   );
 }
 
-function CategoryFilterSection({ isLoading }: FilterSectionProps) {
+function CategoryFilterSection({ isLoading, setFiltersState, filtersState }: FilterSectionProps) {
   const [isShow, setIsShow] = useState(false);
-  const { filterQuery } = useAppSelector((state) => state.filters);
-
-  const ratingQuery = filterQuery.filter((item) => item.hasOwnProperty("category"));
+  const { filtersQuery } = getParams(location);
+  const categoryFilters =
+    filtersQuery && JSON.parse(filtersQuery).filter((item: any) => item.hasOwnProperty("category"));
 
   const transition = useTransition(isShow, {
     from: { height: "0px", opacity: 0, marginBottom: "0px", marginTop: "0px" },
     enter: {
-      height: "400px",
+      height: "600px",
       opacity: 1,
       marginBottom: "20px",
       marginTop: "10px",
@@ -192,7 +225,7 @@ function CategoryFilterSection({ isLoading }: FilterSectionProps) {
       <div className="flex items-center justify-between w-full border-b text-lg">
         <div className="flex gap-1">
           <span>Kategoria</span>
-          {ratingQuery.length > 0 && <span className="text-red-500 text-sm">({ratingQuery.length})</span>}
+          {categoryFilters.length > 0 && <span className="text-red-500 text-sm">({categoryFilters.length})</span>}
         </div>
         <button
           type="button"
@@ -208,7 +241,13 @@ function CategoryFilterSection({ isLoading }: FilterSectionProps) {
             <div className="overflow-hidden">
               <animated.div style={styles} className="flex flex-col gap-1 px-1">
                 {CATEGORY_ARRAY.sort((a, b) => a.localeCompare(b)).map((type) => (
-                  <FilterButton key={type} value={type} disabled={isLoading} />
+                  <FilterButton
+                    key={type}
+                    value={type}
+                    disabled={isLoading}
+                    setFilertsState={setFiltersState}
+                    filtersState={filtersState}
+                  />
                 ))}
               </animated.div>
             </div>
---
+++ frontend/src/components/features/FiltersContainer.tsx
@@ -0,0 +1,52 @@
+import React, { Fragment, useState } from "react";
+import { FaArrowLeft, FaFilter } from "react-icons/fa";
+import { useAppSelector } from "../../redux/store";
+import { useTransition, animated } from "@react-spring/web";
+import Filters from "./Filters";
+import getParams from "../../utils/getUrlParams";
+
+function FilersContainer() {
+  const [isOpen, setIsOpen] = useState(false);
+  const { filtersQuery } = getParams(location);
+
+  const transition = useTransition(isOpen, {
+    from: { transform: "translateX(-100%)" },
+    enter: { transform: "translateX(0%)" },
+    leave: { transform: "translateX(-100%)" },
+  });
+
+  return (
+    <Fragment>
+      <div className="flex flex-col justify-center items-center gap-1">
+        <button onClick={() => setIsOpen((prev) => !prev)} className="flex flex-col items-center gap-1">
+          <div className="flex items-center gap-1">
+            <span className="text-gray-500 dark:text-gray-300 ">Filtry</span>
+            {filtersQuery && <span className="text-red-600">({filtersQuery && JSON.parse(filtersQuery).length})</span>}
+          </div>
+          <FaFilter />
+        </button>
+      </div>
+
+      {transition(
+        (styles, item) =>
+          item && (
+            <animated.div
+              style={styles}
+              className="container mx-auto px-4 pb-4 absolute top-0 left-0 h-[calc(100dvh)] w-screen md:w-[420px] bg-white dark:bg-darkBg overflow-y-auto z-20"
+            >
+              <section className="grid items-center grid-rows-1 grid-cols-4">
+                <button className="text-lg" onClick={() => setIsOpen((current) => !current)}>
+                  <FaArrowLeft />
+                </button>
+                <h2 className="text-center text-lg font-extrabold my-5 col-span-2">Filtry</h2>
+              </section>
+
+              <Filters setIsOpen={setIsOpen} />
+            </animated.div>
+          )
+      )}
+    </Fragment>
+  );
+}
+
+export default FilersContainer;
